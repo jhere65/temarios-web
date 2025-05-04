4b900a6cdb4f8e4c2c350e28b2789dbb281e60d5
jhere65 <126349246+jhere65@users.noreply.github.com>
2025-05-03 21:59:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("¡Bienvenido a Temarios Personalizados!");
 });
 
+document.getElementById("recomendar").addEventListener("click", () => {
+    const edad = parseInt(document.getElementById("edad").value);
+    const resultado = document.getElementById("recomendacion");
+
+    if (!edad || edad < 5) {
+    resultado.textContent = "Por favor, ingresa una edad válida.";
+    return;
+    }
+
+    if (edad <= 11) {
+    resultado.textContent = "Recomendado: Nivel primaria.";
+    } else if (edad <= 16) {
+    resultado.textContent = "Recomendado: Nivel secundaria.";
+    } else {
+    resultado.textContent = "Recomendado: Nivel avanzado o preuniversitario.";
+    }
+});  
+
   // Mostrar/ocultar temarios premium
 document.getElementById("mostrarPremium").addEventListener("click", () => {
     const lista = document.getElementById("listaPremium");
